1e597a9df88f316aff29ebd57628ad56ceadf85e
Jesse Onolememen <jesseono91@gmail.com>
2020-08-06 09:22:03 +0100

    chore(setup): finish tailwindcss setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,32 @@ module.exports = {
         light: { raw: '(prefers-color-scheme: light)' },
         dark: { raw: '(prefers-color-scheme: dark)' },
       },
+      colors: {
+        deepSea: {
+          50: '#F4F4F5',
+          100: '#E8E9EB',
+          200: '#C6C8CE',
+          300: '#A4A7B0',
+          400: '#5F6474',
+          500: '#1B2239',
+          600: '#181F33',
+          700: '#101422',
+          800: '#0C0F1A',
+          900: '#080A11',
+        },
+        nightSky: {
+          50: '#F5F6F6',
+          100: '#ECECED',
+          200: '#CFD0D3',
+          300: '#B1B4B8',
+          400: '#777B83',
+          500: '#3D434E',
+          600: '#373C46',
+          700: '#25282F',
+          800: '#1B1E23',
+          900: '#121417',
+        },
+      },
     },
   },
   variants: {},
@@ -19,7 +45,7 @@ module.exports = {
         '@screen dark': {
           body: {
             color: config('theme.colors.white'),
-            backgroundColor: config('theme.colors.black'),
+            backgroundColor: config('theme.colors.deeps-sea.500'),
           },
         },
       });
